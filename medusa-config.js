ae6a2fee64d2e53cf557c4cb87df140cfff44ab6
hivelydev <torresyang30@hotmail.com>
2024-03-11 23:52:53 -0700

    Updated config

--- medusa-config.js
+++ medusa-config.js
@@ -36,7 +36,6 @@ const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
 const plugins = [
   `medusa-fulfillment-manual`,
   `medusa-payment-manual`,
-  `medusa-plugin-wishlist`,
   {
     resolve: `@medusajs/file-local`,
     options: {
